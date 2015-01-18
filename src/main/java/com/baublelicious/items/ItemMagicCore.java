442aad0d1959ad8c56f1f1c59c189dadd8d92a4e
denbukki <dennishoornaar@live.nl>
2015-01-18 14:48:16 +0100

    fixed dupe glicht

--- src/main/java/com/baublelicious/items/ItemMagicCore.java
+++ src/main/java/com/baublelicious/items/ItemMagicCore.java
@@ -20,6 +20,7 @@ public class ItemMagicCore extends Item {
 	public ItemMagicCore(){
 		super();
 		this.setHasSubtypes(true);
+		this.setMaxStackSize(1);
 		this.setCreativeTab(baublelicious.TabBaublelicious);
 		this.setUnlocalizedName("ItemMagicCore");
 	}
