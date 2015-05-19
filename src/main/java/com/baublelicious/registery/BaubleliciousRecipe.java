26e184ec0f9be04f754257d88804ff7f5c001152
UnwrittenFun <unwrittenfun@gmail.com>
2015-05-19 12:01:47 +0100

    unnecessary object array wrapper

--- src/main/java/com/baublelicious/registery/BaubleliciousRecipe.java
+++ src/main/java/com/baublelicious/registery/BaubleliciousRecipe.java
@@ -32,7 +32,7 @@ public abstract class BaubleliciousRecipe {
     //blocks
 
     // GameRegistry.addRecipe(new ItemStack(BaubleliciousBlocks.BlockPedestal)," G "," p "," Q ", 'g', Blocks.glass, 'Q', Blocks.quartz_block,'p', new ItemStack(Blocks.quartz_block),1,2);
-    GameRegistry.addRecipe(new ItemStack(BaubleliciousBlocks.BlockPedestal), new Object[] { " G ", " p ", "QQQ", 'G', Blocks.glass, 'Q', new ItemStack(Blocks.quartz_block, 1, 0), 'p', new ItemStack(Blocks.quartz_block, 1, 2) });
+    GameRegistry.addRecipe(new ItemStack(BaubleliciousBlocks.BlockPedestal), " G ", " p ", "QQQ", 'G', Blocks.glass, 'Q', new ItemStack(Blocks.quartz_block, 1, 0), 'p', new ItemStack(Blocks.quartz_block, 1, 2));
 
 
   }
