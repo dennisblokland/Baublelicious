b9f24ab5a62e987e083fbf695de3582681d5fa5b
UnwrittenFun <unwrittenfun@gmail.com>
2015-05-19 17:19:45 +0100

    So I never new getDescriptionPacket existed...

--- src/main/java/com/baublelicious/network/receivers/ITileNBTReceiver.java
+++
@@ -1,7 +0,0 @@
-package com.baublelicious.network.receivers;
-
-import net.minecraft.nbt.NBTTagCompound;
-
-public interface ITileNBTReceiver {
-  void onTileNBT(NBTTagCompound compound);
-}
--- src/main/java/com/baublelicious/network/receivers/ITileRequestReceiver.java
+++
@@ -1,7 +0,0 @@
-package com.baublelicious.network.receivers;
-
-import net.minecraft.entity.player.EntityPlayerMP;
-
-public interface ITileRequestReceiver {
-  void onTileRequest(int id, EntityPlayerMP player);
-}
