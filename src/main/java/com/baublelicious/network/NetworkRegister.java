95126ddba693c7126f238b408cabb06db072f891
UnwrittenFun <unwrittenfun@gmail.com>
2015-05-19 11:59:05 +0100

    idk why we are sending packets on SimplyJetpacks' channel...

--- src/main/java/com/baublelicious/network/NetworkRegister.java
+++ src/main/java/com/baublelicious/network/NetworkRegister.java
@@ -1,12 +1,13 @@
 package com.baublelicious.network;
 
+import com.baublelicious.ModInfo;
 import com.baublelicious.handler.MessageToggleGrowthAmulet;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
 
 public class NetworkRegister {
-  public static final SimpleNetworkWrapper wrapper = NetworkRegistry.INSTANCE.newSimpleChannel("SimplyJetpacks");
+  public static final SimpleNetworkWrapper wrapper = NetworkRegistry.INSTANCE.newSimpleChannel(ModInfo.MOD_ID);
 
   public static void registerMessages() {
     wrapper.registerMessage(MessageToggleGrowthAmulet.class, MessageToggleGrowthAmulet.class, 0, Side.SERVER);
