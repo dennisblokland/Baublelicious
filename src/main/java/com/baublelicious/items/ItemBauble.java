ccf97b3e607d1a138e748e5c44123047496f2236
UnwrittenFun <unwrittenfun@gmail.com>
2015-05-20 20:55:44 +0100

    add right click -> equip bauble

--- src/main/java/com/baublelicious/items/ItemBauble.java
+++ src/main/java/com/baublelicious/items/ItemBauble.java
@@ -1,10 +1,14 @@
 package com.baublelicious.items;
 
 import baubles.api.IBauble;
+import baubles.common.container.InventoryBaubles;
+import baubles.common.lib.PlayerHandler;
 import com.baublelicious.Baublelicious;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 // TODO: Move all baubles over to this class
 public abstract class ItemBauble extends Item implements IBauble {
@@ -13,6 +17,33 @@ public abstract class ItemBauble extends Item implements IBauble {
     setTextureName(key);
     setCreativeTab(Baublelicious.tabBaublelicious);
     setMaxStackSize(1);
+  }
+
+  @Override
+  public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+    InventoryBaubles baubles = PlayerHandler.getPlayerBaubles(player);
+    for (int i = 0; i < baubles.getSizeInventory(); i++) {
+      if (baubles.isItemValidForSlot(i, stack)) {
+        ItemStack stackInSlot = baubles.getStackInSlot(i);
+        if (stackInSlot == null || ((IBauble) stackInSlot.getItem()).canUnequip(stackInSlot, player)) {
+          if (!world.isRemote) {
+            baubles.setInventorySlotContents(i, stack.copy());
+            if (!player.capabilities.isCreativeMode)
+              player.inventory.setInventorySlotContents(player.inventory.currentItem, null);
+          }
+
+          onEquipped(stack, player);
+
+          if (stackInSlot != null) {
+            ((IBauble) stackInSlot.getItem()).onUnequipped(stackInSlot, player);
+            return stackInSlot.copy();
+          }
+          break;
+        }
+      }
+    }
+
+    return stack;
   }
 
   @Override
